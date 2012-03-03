1df0e47c2085139c90afd98eadbffdf3b84da6f6
imbakassi <imbakassi@gmail.com>
2012-03-03 00:45:32 +0000

    Baetti vid splash screen

--- Rysc/gen/is/hi/lucky7/R.java
+++ Rysc/gen/is/hi/lucky7/R.java
@@ -13,8 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int Logo=0x7f050000;
+        public static final int btnControls=0x7f050004;
+        public static final int btnNewGame=0x7f050001;
+        public static final int btnNewInternetGame=0x7f050003;
+        public static final int btnRules=0x7f050002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int rulesgui=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Rysc/src/is/hi/lucky7/RyscActivity.java
+++ Rysc/src/is/hi/lucky7/RyscActivity.java
@@ -1,7 +1,9 @@
 package is.hi.lucky7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RyscActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,9 @@ public class RyscActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    public void ViewRules(View view) {
+    	Intent rulesgui = new Intent(getApplicationContext(),
+    			rules.class);
+    	startActivity(rulesgui);
+    } 
 }

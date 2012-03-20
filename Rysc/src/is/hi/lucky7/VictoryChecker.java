3cf974ae88da1484c4d4f74ab71ea50c7286e01d
imbakassi <imbakassi@gmail.com>
2012-03-20 23:42:53 +0000

    lagadi play klasann

--- Rysc/src/is/hi/lucky7/VictoryChecker.java
+++ Rysc/src/is/hi/lucky7/VictoryChecker.java
@@ -1,19 +1,17 @@
 package is.hi.lucky7;
 import java.util.ArrayList;
 
+
 public class VictoryChecker {
-	public static String check(Game game)
+	public int check(Game game)
 	{
 		ArrayList<Player> players = game.getPlayers();
 		ArrayList<Country> countries = game.getCountries();
 		for(int i = 0; i < players.size(); i++) {
 			if(players.get(i).getCountries().size() >= countries.size()){
-				String ret = "-1";
-				if(i < 10) ret += "0";
-				ret += i;
-				return ret;
+				return 1;
 			}
 		}
-		return "0";
+		return 0;
 	}
 }

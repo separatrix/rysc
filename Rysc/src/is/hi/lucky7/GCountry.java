077c35b6b347049e98f796b528faf20f0792260f
Torfi_desktop <torfias@gmail.com>
2012-03-22 00:04:31 +0000

    Added graphics classes

---
+++ Rysc/src/is/hi/lucky7/GCountry.java
@@ -0,0 +1,46 @@
+package is.hi.lucky7;
+
+import java.nio.*;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class GCountry {
+	private float vertices[] = {
+			-1.0f, 1.0f, 0.0f,
+			-1.0f, -1.0f, 0.0f,
+			1.0f, -1.0f, 0.0f,
+			1.0f, 1.0f, 0.0f,
+	};	
+	private short[] indices = { 0,1,2,0,2,3 };	
+	private FloatBuffer vertexBuffer;	
+	private ShortBuffer indexBuffer;
+	
+	public GCountry() {
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+		
+		vbb.order(ByteOrder.nativeOrder());
+		vertexBuffer = vbb.asFloatBuffer();
+		vertexBuffer.put(vertices);
+		vertexBuffer.position(0);
+		
+		ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * 2);
+		ibb.order(ByteOrder.nativeOrder());
+		indexBuffer = ibb.asShortBuffer();
+		indexBuffer.put(indices);
+		indexBuffer.position(0);
+	}
+	
+	public void draw(GL10 gl) {
+		gl.glFrontFace(GL10.GL_CCW);
+		gl.glEnable(GL10.GL_CULL_FACE);
+		gl.glCullFace(GL10.GL_BACK);
+		
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+		
+		gl.glDrawElements(GL10.GL_TRIANGLES, indices.length, GL10.GL_UNSIGNED_SHORT, indexBuffer);
+		
+		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisable(GL10.GL_CULL_FACE);
+	}
+}
---
+++ Rysc/src/is/hi/lucky7/OpenGLRenderer.java
@@ -0,0 +1,44 @@
+package is.hi.lucky7;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+// import android.opengl.GLU; surfaceChanged
+import android.opengl.GLU;
+
+public class OpenGLRenderer implements Renderer {
+	private GCountry country;
+	
+	public OpenGLRenderer() {
+		country = new GCountry();
+	}
+	
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		gl.glClearColor(0.0f, 0.0f, 0.3f, 0.5f);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glClearDepthf(1.0f);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL10.GL_LEQUAL);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+	}
+	
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		
+		gl.glLoadIdentity();
+		
+		gl.glTranslatef(0, 0, -10);
+		
+		country.draw(gl);
+	}
+	
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+	}
+}

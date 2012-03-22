0291eb971a1ee45f45649ebaab49d0ef54ce9e9f
Torfi_desktop <torfias@gmail.com>
2012-03-22 00:09:56 +0000

    bull

--- Rysc/src/is/hi/lucky7/RyscActivity.java
+++ Rysc/src/is/hi/lucky7/RyscActivity.java
@@ -1,7 +1,9 @@
 package is.hi.lucky7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RyscActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +12,40 @@ public class RyscActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    public void ViewRules(View view) {
+    	Intent rulesgui = new Intent(getApplicationContext(),
+    			rules.class);
+    	startActivity(rulesgui);
+    } 
+    
+    public void StartGame(View view) {
+    	Intent StartGame = new Intent(getApplicationContext(),
+    			StartGameActivity.class);
+    	startActivity(StartGame);
+    } 
+    
+    public void JoinGame(View view) {
+    	Intent JoinGame = new Intent(getApplicationContext(),
+    			JoinGameActivity.class);
+    	startActivity(JoinGame);
+    } 
+    
+    public void UdpSend(View view) {
+    	Intent UdpSend = new Intent(getApplicationContext(),
+    			UdpSendActivity.class);
+    	startActivity(UdpSend);
+    }
+        
+    public void UdpReceive(View view) {
+       	Intent UdpReceive = new Intent(getApplicationContext(),
+       			UdpReceiveActivity.class);
+       	startActivity(UdpReceive);
+    } 
+    
+    public void TextGame(View view) {
+       	Intent Play = new Intent(getApplicationContext(),
+       			Play.class);
+       	startActivity(Play);
+    } 
 }

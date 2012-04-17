0bb278e7d58d68d21dbe36f1943187da2ef626bf
unknown <wintermute@.(none)>
2012-04-17 23:13:20 +0000

    Added some text.

--- Rysc/src/is/hi/lucky7/GMapView.java
+++ Rysc/src/is/hi/lucky7/GMapView.java
@@ -5,19 +5,45 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
 public class GMapView extends View {
 	private Bitmap bmp;
 	private WindowManager wm;
+	private Paint paint;
+	private TextView t_one;
+	private int countryCoords[] = {	46,60,
+									123,62,
+									283,47,
+									229,114,
+									174,109,
+									115,105,
+									127,158,
+									183,175,
+									118,210
+									};
 	
 	public GMapView(Context context, WindowManager _wm) {
 		super(context);
 		bmp = BitmapFactory.decodeResource(getResources(), R.drawable.map1);
 		wm = _wm;
+		
+		t_one = new TextView(context);
+		
+		paint = new Paint();
+		paint.setTextSize(32f);
+		paint.setAntiAlias(true);
+		paint.setFakeBoldText(true);
+		paint.setStyle(Paint.Style.FILL);
+		paint.setTextAlign(Paint.Align.LEFT);
+		paint.setColor(Color.WHITE);
 	}
 	
 	protected void onDraw(Canvas canvas) {		
@@ -30,5 +56,23 @@ public class GMapView extends View {
 		canvas.drawColor(Color.BLACK);
 		canvas.scale(scaleW,scaleH);
 		canvas.drawBitmap(bmp,0,0,null);
+		
+		for(int i=0; i<countryCoords.length; i +=2) {
+			if (i==0) {
+				paint.setColor(Color.BLACK);
+			}
+			else {
+				paint.setColor(Color.WHITE);
+			}
+//			canvas.drawText("10",countryCoords[i],countryCoords[i+1],paint);
+			canvas.drawText("10",countryCoords[i],countryCoords[i+1],paint);
+		}
+	}
+	
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d("Hey","Touched");
+		Log.d("X", ""+event.getX());
+		Log.d("Y", ""+event.getY());
+		return true;
 	}
 }

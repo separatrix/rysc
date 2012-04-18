235369ff069b94b778ec9fab73b9bbb7db1add6f
Torfi_desktop <torfias@gmail.com>
2012-04-18 04:46:01 +0000

    Added text and circles.

--- Rysc/src/is/hi/lucky7/GMapView.java
+++ Rysc/src/is/hi/lucky7/GMapView.java
@@ -1,5 +1,7 @@
 package is.hi.lucky7;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,17 +20,56 @@ public class GMapView extends View {
 	private Bitmap bmp;
 	private WindowManager wm;
 	private Paint paint;
+	private Paint paintB;
 	private TextView t_one;
-	private int countryCoords[] = {	46,60,
-									123,62,
-									283,47,
-									229,114,
-									174,109,
-									115,105,
-									127,158,
-									183,175,
-									118,210
+	private int countryCoords[] = {	75,113,		// Alaska
+									185,115,	// No. Canada
+									425,95,		// Greenland
+									180,190,	// SW Canada
+									260,210,	// S Canada
+									350,200,	// E Canada
+									180,290,	// W Usa
+									280,320,	// E Usa
+									185,380,	// Mexico
+									280,480,	// Colombia
+									385,580,	// Brazil
+									310,590,	// Peru
+									320,680,	// Chile
+									540,150,	// Iceland
+									630,160,	// Norway
+									510,260,	// UK
+									630,285,	// Baltic
+									730,220,	// W Russia
+									530,400,	// Spain
+									630,370,	// Italy
+									570,540,	// W Africa
+									670,490,	// Egypt
+									720,560,	// Horn
+									670,630,	// C Africa
+									670,740,	// S Africa
+									780,740,	// Madagascar
+									760,440,	// M East
+									840,320,	// Kazakh
+									900,440,	// India
+									980,380,	// China
+									870,220,	// C Russia
+									930,150,	// N Russia
+									990,480,	// Thailand
+									1010,300,	// Korea
+									1000,230,	// SE Russia
+									1030,110,	// NE Russia
+									1110,120,	// E Russia
+									1130,310,	// Japan
+									1010,620,	// Phillipines
+									1110,590,	// New Zealand
+									1120,690,	// E Aus
+									1040,720,	// W Aus
 									};
+	
+	private ArrayList<Country> countries;
+	private ArrayList<Player> players;
+	
+	private int phase = 0; // 0 = Not your turn, 1 = Reinforce, 2 = Attack
 	
 	public GMapView(Context context, WindowManager _wm) {
 		super(context);
@@ -42,8 +83,14 @@ public class GMapView extends View {
 		paint.setAntiAlias(true);
 		paint.setFakeBoldText(true);
 		paint.setStyle(Paint.Style.FILL);
-		paint.setTextAlign(Paint.Align.LEFT);
+		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setColor(Color.WHITE);
+		
+		paintB = new Paint();
+		paintB.setAntiAlias(true);
+		paintB.setStyle(Paint.Style.FILL);
+		paintB.setColor(Color.BLACK);
+		paintB.setAlpha(200);
 	}
 	
 	protected void onDraw(Canvas canvas) {		
@@ -57,15 +104,31 @@ public class GMapView extends View {
 		canvas.scale(scaleW,scaleH);
 		canvas.drawBitmap(bmp,0,0,null);
 		
+		String armies = "0";
+		
 		for(int i=0; i<countryCoords.length; i +=2) {
-			if (i==0) {
-				paint.setColor(Color.BLACK);
+			switch (countries.get(i/2).getOwner().getId()) {
+				case 0:
+					paintB.setColor(Color.BLUE);
+					break;
+				case 1:
+					paintB.setColor(Color.RED);
+					break;
+				case 2:
+					paintB.setColor(Color.GREEN);
+					break;
+				case 3:
+					paintB.setColor(Color.YELLOW);
+					break;
+				default:
+					paintB.setColor(Color.BLACK);
+					break;
 			}
-			else {
-				paint.setColor(Color.WHITE);
-			}
-//			canvas.drawText("10",countryCoords[i],countryCoords[i+1],paint);
-			canvas.drawText("10",countryCoords[i],countryCoords[i+1],paint);
+			
+			armies = "" + countries.get(i/2).getArmies();
+			
+			canvas.drawCircle(countryCoords[i]+2, countryCoords[i+1]-8, 35, paintB);
+			canvas.drawText(armies,countryCoords[i],countryCoords[i+1],paint);
 		}
 	}
 	
